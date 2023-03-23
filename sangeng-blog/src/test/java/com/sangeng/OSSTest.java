d3e33c7d76fdd30006b4f7f254faefd432490534
高莹豪 <yinghaogao097@gmail.com>
2023-03-23 14:49:49 +0800

    去除Key

--- sangeng-blog/src/test/java/com/sangeng/OSSTest.java
+++ sangeng-blog/src/test/java/com/sangeng/OSSTest.java
@@ -23,8 +23,8 @@ public class OSSTest {
         // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
         String endpoint = "https://oss-cn-beijing.aliyuncs.com";
         // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "LTAI5tG51FwFqCcXXHa8D78i";
-        String accessKeySecret = "1sDUmjGcUPfbMpzzZNDT5nE3PHMC7y";
+        String accessKeyId = "";
+        String accessKeySecret = "";
         // 填写Bucket名称，例如examplebucket。
         String bucketName = "achen1117";
         // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
