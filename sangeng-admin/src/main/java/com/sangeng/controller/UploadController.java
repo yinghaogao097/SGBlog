8b66894713783b9d3ab9aa0c2ec44921b638d512
高莹豪 <yinghaogao097@gmail.com>
2023-03-31 10:11:15 +0800

    上传图片接口完成

---
+++ sangeng-admin/src/main/java/com/sangeng/controller/UploadController.java
@@ -0,0 +1,28 @@
+package com.sangeng.controller;
+
+import com.sangeng.domain.ResponseResult;
+import com.sangeng.service.UploadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author Achen
+ */
+@RestController
+public class UploadController {
+    @Autowired
+    private UploadService uploadService;
+
+    /**
+     * 上传图片
+     *
+     * @param img 图片
+     * @return
+     */
+    @PostMapping("/upload")
+    public ResponseResult uploadImg(MultipartFile img) {
+        return uploadService.uploadImg(img);
+    }
+}
